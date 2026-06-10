6a9889b87b7203ca859a39e9774c26c5a8afe239
Sylvernus Akubo <akubosylvernus@gmail.com>
2026-06-10 14:31:28 +0100

    update

--- lib/app-links.ts
+++ lib/app-links.ts
@@ -1,3 +1,3 @@
-export const MELORA_APP_URL = "https://app.melorahealth.io/";
+export const MELORA_APP_URL = "https://app.melorahealth.io/signup";
 
 export const MELORA_GIFT_URL = "https://app.melorahealth.io/gift";
